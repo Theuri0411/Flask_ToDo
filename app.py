3dfa35cc0fc6f17493f0dfe72c945dcc951bfdfd
Theuri0411 <theuri007@students.uonb.ac.ke>
2022-03-23 02:56:32 +0300

    Setting database

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+from turtle import title
 from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
 
@@ -8,13 +9,32 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
+
+class ToDo (db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column (db.String(100))
+    complete - db.Column (db.Boolean)
+    
+
 @app.route("/")
 def  index():
+    #Show all database
+    
+    todo_list = ToDo.query.all()
+    print (todo_list)
     return render_template("base.html")
-
 
 
 
 if __name__ == "__main__":
     db.create_all()
+    
+    new_todo = ToDo(title='todo_app', complete = False)
+    
+    db.session.add (new_todo)
+    db.session.commit()
+    
+    
     app.run (debug=True)
+    
+    
